f8e67ba837ca41921ce21cbeac7c8b12523bd4ea
hilaldeniz1 <hilaldeniz677@gmail.com>
2023-08-13 14:32:09 +0300

    hava durumuna göre icon eklendi

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-const url = 'https://api.openweathermap.org/data/2.5/'
-const key = '7994916e07a2aaf37f9f5fa8d5cc0570'
+const url = "https://api.openweathermap.org/data/2.5/";
+const key = "7994916e07a2aaf37f9f5fa8d5cc0570";
 
-const setQuery = (e) => {
-    if(e.keyCode == '13')
-    getResult(searchBar.value)
+const box = document.querySelector(".weather-box");
+const searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("keypress", setQuery);
+
+function setQuery(e) {
+  if (e.key === "Enter") {
+    getResult(searchBar.value);
+  }
 }
 
-const getResult = (cityName) => {
-    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
-    fetch(query)
-    .then(weather => {
-        return weather.json()
+function getResult(cityName) {
+  let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
+  fetch(query)
+    .then((weather) => {
+      return weather.json();
     })
-    .then(displayResult)
+    .then(displayResult);
 }
 
 const displayResult = (result) => {
-    let city = document.querySelector('.city')
-    city.innerText = `${result.name},${result.sys.country}`
+  let city = document.querySelector(".city");
+  city.innerText = `${result.name},${result.sys.country}`;
 
-    let temp = document.querySelector('.temp')
-    temp.innerText = `${Math.round(result.main.temp)}°c`
+  let temp = document.querySelector(".temp");
+  temp.innerText = `${Math.round(result.main.temp)}°C`;
 
-    let desc = document.querySelector('.desc')
-    desc.innerText = result.weather[0].description
+  let desc = document.querySelector(".desc");
+  desc.innerText = result.weather[0].description;
 
-    let minmax = document.querySelector('.minmax')
-    minmax.innerText = `${Math.round(result.main.temp_min)}°c /
-    ${Math.round(result.main.temp_max)}°c`
-}
+  let minmax = document.querySelector(".minmax");
+  minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(
+    result.main.temp_max
+  )}°C`;
 
-const searchBar = document.getElementById('searchBar')
-searchBar.addEventListener('keypress',setQuery)
+  const image = document.createElement("img");
+  console.dir(result);
+  // switch case ile hava durumuna göre resim ekledim
+  switch (result.weather[0].description) {
+    case "açık":
+      image.src = "günes.png";
+      break;
+    case "parçalı bulutlu":
+      image.src = "kapali.png";
+      break;
+    case "az bulutlu":
+      image.src = "kapalii.png";
+      break;
+    case "kapalı":
+      image.src = "kapalii.png";
+      break;
+    case "kar yağışlı":
+      image.src = "karli.png";
+      break;
+    case "yagmurlu":
+      image.src = "yagmur.png";
+      break;
+    // Diğer hava durumları için eklemeler yapabilirsiniz
+    default:
+      image.src = ""; // Varsayılan durumda resmi kaldırabilirsiniz
+      break;
+  }
+  box.innerHTML = "";
+  box.appendChild(image);
+};
